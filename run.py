b4721d954c8dc16cf7ffe1206886653c953c7132
Dominik Korsa <dominik.korsa@gmail.com>
2023-09-05 17:52:45 +0200

    Add VLOTT_PORT env variable

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ def envor(x, y):
 LEGACY_ADDR = envor("VLOTT_LEGACY_ADDR", "127.0.0.1:7002")
 WORKERS = envor("VLOTT_WORKERS", "1")
 V1_ENABLED = envor("VLOTT_USE_V1", "1")
+PORT = envor("VLOTT_PORT", "80")
 
 if __name__ == "__main__":
-	os.system(f"gunicorn -k uvicorn.workers.UvicornWorker app:app --log-level warn -w {WORKERS} -b 0.0.0.0:80")
+	os.system(f"gunicorn -k uvicorn.workers.UvicornWorker app:app --log-level warn -w {WORKERS} -b 0.0.0.0:{PORT}")
